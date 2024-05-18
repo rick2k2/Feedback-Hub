bab5f133179b68cb6d0b1007e0051750ed6e7bdd
Rick saha <68051340+rick2k2@users.noreply.github.com>
2024-05-18 18:45:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
 const icons = document.querySelectorAll(".icon");
 const send_btn = document.querySelector("#send_btn");
 const main = document.querySelector(".main_container");
+const app_heading = document.querySelector(".app_heading");
 let icon_name = "";
 
 icons.forEach((icon) => {
   icon.addEventListener("click", () => {
     icon_name = icon.id;
-    get_icon_name(icon_name);
+    let icon_emoji;
+    if (icon_name === "Unhappy") {
+      icon_emoji = "😒";
+    } else if (icon_name === "Neutral") {
+      icon_emoji = "😐";
+    } else if (icon_name === "Satisfied") {
+      icon_emoji = "😆";
+    }
+    app_heading.innerHTML = `Your Feedback is ${icon_name} ${icon_emoji}`;
+    get_icon_name(icon_name, icon_emoji);
   });
 });
 
-function get_icon_name(icon_name) {
-  console.log(icon_name);
+function get_icon_name(icon_name, icon_emoji) {
   send_btn.addEventListener("click", () => {
-    console.log("send button fired");
-    let icon;
-    if (icon_name === "Unhappy") {
-      icon = "😒";
-    } else if (icon_name === "Neutral") {
-      icon = "😐";
-    } else if (icon_name === "Satisfied") {
-      icon = "😆";
-    }
     main.innerHTML = `<html>
         <span>💖</span>
         <span>Thank You!</span>
-        <span>Feedback: ${icon_name} ${icon}</span>
+        <span>Feedback: ${icon_name} ${icon_emoji}</span>
         <span>copyright: @dev_ricksaha2k24</span>
       </html>`;
-
-    console.log(main.innerHTML);
     const spans = document.querySelectorAll("span");
     for (let i = 0; i < spans.length; i++) {
       spans[i].classList.add("span_zoom");
